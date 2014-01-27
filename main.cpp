5aa321db567a3f15c306ae84d6fcb11af2250e07
Wander Torres <wander_torres@live.com>
2014-01-27 04:23:06 -0300

    Código pronto faltando tratamento de excessões e documentação - PILHA

--- main.cpp
+++ main.cpp
@@ -4,52 +4,81 @@
  *
  * Created on 23 de Janeiro de 2014, 20:58
  */
-
-// class templates
 #include <iostream>
 using namespace std;
 
 template <class T>
 class StackGe {
-    int max, head;
-    T stack[];
+    int head, max;
+    T stack[10];
   public:
     StackGe() {
         max = 10;
         head = 0;
     } 
-    StackGe(T max) {
-        this->max;
+
+    StackGe(int max) {
+        this->max = max;
         head = 0;
     }
-    bool push(T value);
+    bool isEmpty();
     bool isFull();
+    int size();
     T top();
+    void push(T value);
+    T pop();
+    void clear();
+    T print();
 };
-
 template <class T>
-void StackGe<T>::push (T value) {
-    stack[++head] = value;
-}
-
-template <class T>
-bool StackGe<T>::isFull() {
-    return size==head;
+bool StackGe<T>::isEmpty() {
+    return head==0;
 }
 
 template <class T>
 T StackGe<T>::top() {
-    return stack[head];
+    if(isEmpty() == false)
+        return stack[head];
+}
+
+template <class T>
+bool StackGe<T>::isFull() {
+    return head==max;
+}
+
+template <class T>
+int StackGe<T>::size() {
+   return head; 
+}
+
+template <class T>
+void StackGe<T>::push(T value) {
+    //Adicionar tratamento de exceção
+    stack[++head] = value;
+}
+
+template <class T>
+T StackGe<T>::pop() {
+    //Adicionar tratamento de exceção    
+    return stack[head--];
+}
+
+template <class T>
+void StackGe<T>::clear() {
+    head = 0;
+}
+
+template <class T>
+T StackGe<T>::print() {
+    while(isEmpty() == false) {
+        cout << pop() << endl; 
+    }
 }
 
 int main () {
-    StackGe<int> s;
-    s.push(2);  
-    s.isFull();
-    cout << s.top() << endl;
-    s.push(3);
-    cout << s.top() << endl;
-    
+  StackGe<int> s;  
+  
   return 0;
 }
+
 
